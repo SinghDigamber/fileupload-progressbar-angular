38ed4891db54f2cda93d21e2ecb2fb3b6fef2b19
remotestack377 <digamber@positronx.io>
2023-07-09 14:35:57 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { FileUploadService } from './file-upload.service';
+import { HttpEvent, HttpEventType } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
+
 export class AppComponent {
-  title = 'fileupload-progressbar-angular';
+  form: FormGroup;
+  progress: number = 0;
+
+  constructor(
+    public fb: FormBuilder,
+    public fileUploadService: FileUploadService
+  ) {
+    this.form = this.fb.group({
+      name: [''],
+      avatar: [null],
+    });
+  }
+
+  ngOnInit() {}
+
+  uploadFile(event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({
+      avatar: file,
+    });
+    this.form.get('avatar').updateValueAndValidity();
+  }
+
+  submitUser() {
+    this.fileUploadService
+      .addUser(this.form.value.name, this.form.value.avatar)
+      .subscribe((event: HttpEvent<any>) => {
+        switch (event.type) {
+          case HttpEventType.Sent:
+            console.log('Request has been made!');
+            break;
+          case HttpEventType.ResponseHeader:
+            console.log('Response header has been received!');
+            break;
+          case HttpEventType.UploadProgress:
+            this.progress = Math.round((event.loaded / event.total) * 100);
+            console.log(`Uploaded! ${this.progress}%`);
+            break;
+          case HttpEventType.Response:
+            console.log('User successfully created!', event.body);
+            setTimeout(() => {
+              this.progress = 0;
+            }, 1500);
+        }
+      });
+  }
 }
---
+++ src/app/file-upload.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { HttpErrorResponse, HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FileUploadService {
+  constructor(private http: HttpClient) {}
+
+  addUser(name: string, profileImage: File): Observable<any> {
+    var formData: any = new FormData();
+    formData.append('name', name);
+    formData.append('avatar', profileImage);
+
+    return this.http
+      .post('http://localhost:4000/api/create-user', formData, {
+        reportProgress: true,
+        observe: 'events',
+      })
+      .pipe(catchError(this.errorMgmt));
+  }
+
+  errorMgmt(error: HttpErrorResponse) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(() => {
+      return errorMessage;
+    });
+  }
+}
